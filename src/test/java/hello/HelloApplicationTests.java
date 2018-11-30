5c9dee48164ab34df8c9aac76d852b4ea894e902
intachompoo <38059854+intachompoo@users.noreply.github.com>
2018-11-30 10:22:11 +0700

    Update HelloApplicationTests.java

--- src/test/java/hello/HelloApplicationTests.java
+++ src/test/java/hello/HelloApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest

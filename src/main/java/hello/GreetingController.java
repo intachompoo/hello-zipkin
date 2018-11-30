94e11cf72ce68c75d914ee5a5bf4fae9cfb2cbef
intachompoo <38059854+intachompoo@users.noreply.github.com>
2018-11-30 14:22:03 +0700

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 
 @RestController
 public class GreetingController {
